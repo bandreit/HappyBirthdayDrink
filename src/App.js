b78879116c1674a69bdf7c368effaaae4d99b540
Andrei Bostan <abostan.ir@gmail.com>
2020-08-28 15:01:09 +0200

    added party animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
+import party from "./images/party.svg";
 import body from "./images/corp.png";
 import hand from "./images/mana.png";
 import mouth from "./images/gura.png";
@@ -8,6 +9,7 @@ import "./App.css";
 
 function App() {
   const [rotate, setRotate] = useState(0);
+  const [rotateMouth, setRotateMouth] = useState(0);
   const [top, setTop] = useState(116);
   const [disableButton, setDisableButton] = useState(false);
 
@@ -15,10 +17,12 @@ function App() {
     setDisableButton(true);
     setRotate(rotate + 105);
     setTop(top + 15);
+    setRotateMouth(rotateMouth - 40);
 
     setTimeout(function () {
       setRotate(rotate);
       setTop(top);
+      setRotateMouth(rotateMouth);
     }, 3000);
 
     setTimeout(function () {
@@ -40,12 +44,29 @@ function App() {
         />
         <motion.img
           src={mouth}
-          animate={{ top: top }}
+          style={{ originX: 0.6, originY: 0.5 }}
+          animate={{ rotate: rotateMouth }}
           transition={{ duration: 3 }}
           alt="mouth"
           className="mouth"
         />
       </div>
+      <motion.div
+        className="card"
+        animate={{
+          scale: [1, 1.4, 1.4, 1, 1],
+          rotate: [0, 0, 270, 270, 0],
+        }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut",
+          times: [0, 0.2, 0.5, 0.8, 1],
+          loop: Infinity,
+          repeatDelay: 1,
+        }}
+      >
+        <img src={party} alt="Party" />
+      </motion.div>
       <div className="button-div">
         <button
           disabled={disableButton}
